e49a3a0018f0d8e93e81dbcf68b487618c2428f1
kumoroha <199690710+kumoroha@users.noreply.github.com>
2025-12-16 13:25:05 +0900

    Update ohm.js

--- ohm.js
+++ ohm.js
@@ -5,3 +5,15 @@ let ans;
 let voltbar = document.getElementById('dcv');
 let ampr = document.getElementById('i');
 let vfr = document.getElementById('vf');
+
+function cal() {
+    voltba = voltbar.value;
+    amp = ampr.value;
+    vf = vfr.value;
+    if (voltba == null || voltba == '' || amp == null || amp = '' || vf == null || vf == '') {
+        alert('すべてのテキストボックスに値を入力してください。');
+    } else {
+        ans = (voltba - vf) / amp;
+    }
+    ansar.textContent = ans;
+}
