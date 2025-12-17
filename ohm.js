7f3551654a5e3830c31c3bb1773242c724fce13b
kumoroha <199690710+kumoroha@users.noreply.github.com>
2025-12-17 13:17:36 +0900

    Update ohm.js

--- ohm.js
+++ ohm.js
@@ -6,6 +6,7 @@ let voltbar = document.getElementById('dcv');
 let ampr = document.getElementById('i');
 let vfr = document.getElementById('vf');
 let w;
+let mes;
 const ansar = document.getElementById('ansdiv');
 
 voltbar.focus();
@@ -19,10 +20,16 @@ function cal() {
     } else if (amp == 0) {
         alert('アンペア数を確認してください。\n本当に0ですか？');
     } else {
+        amp = amp * 1000;
         ans = (voltba - vf) / amp;
         w = vf * amp;
+        if (w < 0.15) {
+            mes = '1/4Wのカーボン抵抗で大丈夫です。';
+        } else {
+            mes = '1/2Wの金属皮膜抵抗やそれ以上の電気量に耐えられる抵抗をお勧めします。';
+        }
     }
-    ansar.textContent = `結果：${ans}Ω,${w}W`;
+    ansar.textContent = `結果：${ans}Ω,${w}W　${mes}`;
 }
 
 voltbar.addEventListener('input' ,(event) => {
@@ -34,3 +41,4 @@ ampr.addEventListener('input', (event) => {
 vfr.addEventListener('input', (event) => {
     cal();
 });
+
